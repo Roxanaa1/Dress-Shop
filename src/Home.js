f6c56f58478f357ae03fe0e0162d617f4c39f5eb
Roxanaa1 <roxanaenache2003@gmail.com>
2024-07-30 12:48:41 +0300

    Update Home page

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import Navbar from './Navbar';
 import './Home.css';
+import Navbar from './Navbar';
+
+const dresses = [
+    { src: '/images/Dress1.png', name: 'ROCHIE MIDI ELEGANTA CU FUNDE DECORATIVE PE UMERI', price: '200 Lei' },
+    { src: '/images/Dress2.png', name: 'ROCHIE CU MANECI SCURTE BRODATE CU PERLE', price: '150 Lei' },
+    { src: '/images/Dress3.png', name: 'ROCHIE CU DECOLTEU PATRAT SI PERLE', price: '180 Lei' }
+];
 
 const Home = () => {
     return (
         <div className="Home">
             <Navbar />
             <main>
-
                 <h1>Welcome to Home Page</h1>
+                <div className="image-gallery">
+                    {dresses.map((dress, index) =>
+                        (
+                        <div key={index} className="image-wrapper">
+                            <img src={dress.src} alt={`Dress ${index}`} />
+                            <div className="dress-info">
+                                <p className="dress-name">{dress.name}</p>
+                                <p className="dress-price">{dress.price}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </main>
         </div>
     );
